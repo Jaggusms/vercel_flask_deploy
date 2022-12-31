e592f1c797423c5c332478a6e8d21869db92e6a5
Jagadeesh <sanniboinajagadeesh@gmail.com>
2022-12-31 15:59:56 +0530

    login

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
-from flask import Flask
-
+from flask import Flask,request,render_template 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h2>Flask Vercel</h2>"
+    return render_template('home.html',name="jagadeesh")
