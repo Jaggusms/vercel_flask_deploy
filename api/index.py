39b6ef26a7c82c108fd9bd92f833589d95df2182
Jagadeesh <sanniboinajagadeesh@gmail.com>
2022-12-31 15:24:03 +0530

    login

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler
 
-app = Flask(__name__)
+class handler(BaseHTTPRequestHandler):
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
