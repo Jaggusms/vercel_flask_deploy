6ea5f2b7a301d0c2deb855462b4e1289659f6ff4
Jagadeesh <sanniboinajagadeesh@gmail.com>
2022-12-31 19:44:57 +0530

    login

--- index.py
+++ index.py
@@ -1,2 +1,20 @@
 import streamlit as st
-st.title("hello")
+def main():
+    st.title("simple login form")
+    menu=["Home","login","Signup"]
+    choice=st.sidebar.selectbox("menu",menu)
+    if choice=="Home":
+        st.subheader("Home")
+    elif choice=="login":
+        st.subheader("Login")
+        username=st.sidebar.text_input("User Name")
+        password=st.sidebar.text_input("password",type="password")
+        if st.sidebar.button("login"):
+            st.success(f" login in as {username}")
+        
+        
+    else:
+        st.subheader("SignUp")
+        
+if __name__=="__main__":
+    main()
